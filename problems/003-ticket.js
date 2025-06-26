da3e8138115d388b00f24afe6018d6e316aa9391
SwarmGost404 <vik.z.c.r@gmail.com>
2025-06-26 06:22:05 +0300

    therst

--- problems/003-ticket.js
+++ problems/003-ticket.js
@@ -14,7 +14,14 @@
  * @returns {boolean}
  */
 function checkTicket(number) {
-    return undefined;
+    const fistHalph = number.slice(0, 3).split('')
+    const secondHalph = number.slice(3, 6).split('')
+    const res1 = fistHalph.reduce((acc, num) => acc + Number(num), 0);
+    const res2 = secondHalph.reduce((acc, num) => acc + Number(num), 0);
+    if (res1 === res2) {
+        return true
+    }
+    return false
 }
 
 module.exports = checkTicket;
